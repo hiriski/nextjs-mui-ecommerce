75393c4b8d9cf02240db483bab3d744c9bccb002
hiriski <hiriski@outlook.com>
2023-05-23 21:40:55 +0700

    Install packages & update to nextjs 13

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
+
+const { i18n } = require('./next-i18next.config')
+
+const nextConfig = {
   reactStrictMode: true,
+  env: {
+    // prettier-ignore
+    PUBLIC_URL: process.env.NODE_ENV === 'production' ? process.env.BASE_URL : 'http://localhost:3000',
+  },
+  i18n,
 }
+
+module.exports = nextConfig
